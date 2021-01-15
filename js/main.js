56f7759dca8d5dcd6b627f1084736612d5f2b6df
Sam Kao <Sam.Kao@acer.com>
2021-01-15 11:49:13 +0800

    修正驗票人員管理

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ Vue.component('top-bar',{
                </div>`,
     data: function(){
         return {
-            title: "AI驗票機後台系統 v1.18"
+            title: "AI驗票機後台系統 v1.19"
         }
     }
 })
